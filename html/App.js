d08f4c2f14e16b2a244efc5aebba810cdcd1ccc9
himasnsh <himanshhiphop11@gmail.com>
2024-09-30 06:44:28 -0700

    see

---
+++ html/App.js
@@ -0,0 +1,15 @@
+import React from 'React'
+import {Text,View} from 'react-native'
+
+const App=()=>{
+    return(
+        <View>
+            <Text>jack</Text>
+            <Text>raj</Text>
+            <Text>himansh</Text>
+
+        </View>
+    )
+}
+
+export default App
